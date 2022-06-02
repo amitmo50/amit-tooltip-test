22fb8481f600cd9234a2f7418f2bc8cd471f8fbb
amitmo50 <amitmo50@gmail.com>
2022-06-02 12:11:52 +0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,13 +16,15 @@ export class AppComponent implements OnInit, OnDestroy {
   ];
 
   optionsTwo = [
-    { id: '1', label: 'Width: 300' },
-    { id: '2', label: 'Width: 400' },
-    { id: '3', label: 'Width: 500' },
+    { id: '1', label: 'Change Width' },
+    { id: '2', label: 'Change Height' },
+    { id: '3', label: 'Change Width & Height' },
+    { id: '4', label: 'Change Color' },
+    { id: '5', label: 'Default' },
   ];
 
   formControl = new FormControl({ id: '1', label: 'Tooltip Left' });
-  formControlConfig = new FormControl({ id: '1', label: 'Tooltip Left' });
+  formControlConfig = new FormControl({ id: '5', label: 'Default' });
   config$ = new BehaviorSubject<any>(null);
   showTooltipOption$ = new BehaviorSubject<any>(null);
 
@@ -45,6 +47,26 @@ export class AppComponent implements OnInit, OnDestroy {
       )
       .subscribe((val) => {
         this.showTooltipOption$.next(val);
+      });
+
+    this.formControlConfig.valueChanges
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe((val) => {
+        let config;
+        switch (val.id) {
+          case '1':
+            config = { width: 200 };
+            this.config$.next(config);
+            break;
+          case '2':
+            config = { height: 300 };
+            this.config$.next(config);
+            break;
+          case '3':
+            config = { ...this.config$.getValue(), height: 300, width: 200 };
+            this.config$.next(config);
+            break;
+        }
       });
   }
 }
