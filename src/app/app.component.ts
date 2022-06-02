e9958db6355586a45cae866baec76bae87d02f46
amitmo50 <amitmo50@gmail.com>
2022-06-02 11:09:13 +0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  options = [
+    { id: 1, label: 'Option A', tooltip: 'ToolTip A', disabled: true },
+    { id: 2, label: 'Option B', tooltip: 'ToolTip B' },
+    { id: 3, label: 'Option C', tooltip: 'ToolTip C' },
+  ];
+  formControl = new FormControl({ id: 2, label: 'Checked B' });
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ButtonModule } from '@ironsource/fusion-ui/components/button';
-
+import { ButtonModule } from '@ironsource/fusion-ui/components/button/v3';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SvgModule } from '@ironsource/fusion-ui/components/svg';
 import { TooltipModule } from '@ironsource/fusion-ui/components/tooltip/v3';
 import { IconModule } from '@ironsource/fusion-ui/components/icon';
+import { RadioGroupModule } from '@ironsource/fusion-ui/components/radio-group';
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     ButtonModule,
     SvgModule.forRoot({
       assetsPath: 'https://fusion.ironsrc.net/assets',
     }),
     IconModule,
     TooltipModule,
+    RadioGroupModule,
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
